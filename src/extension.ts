eef4c80a938381e1012e63839bab46a74f28dfa7
larry <1762861715@qq.com>
2024-07-26 14:56:41 +0800

    动态按钮

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,69 @@
 
 import * as vscode from 'vscode';
 import * as tool from './tool';
-let btnInfo = {
-    action: "tool.showMessage('Hello World!');",
-    action2:"await tool.execFn('node 全路径地址')",
-    data: { key: 'name' }
-};
+import * as data from './btn.json';
 
 
-function initBtn(){
+let actionMap = new Map();
+function initBtn() {
+    // 从配置读取
+    let myButton;
+    data.btnList.forEach(item => {
+        myButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+        myButton.tooltip = item.name;
+        myButton.text = `${item.icon}`;
+        myButton.color = 'white';
+        myButton.command = {
+            title: '',
+            command: 'dy-btn.action',
+            arguments: [item.id]
+        };
+        myButton.show();
+        actionMap.set(item.id, item);
+    });
+
 
 }
+function showAddDialog() {
 
+}
 export function activate(context: vscode.ExtensionContext) {
     // 展示动态按钮
     initBtn();
 
     // 注册按钮事件
-	let disposable = vscode.commands.registerCommand('dy-btn.helloWorld', async () => {
-        await tool.execFn('echo 1');
-       
-       
-
+    let disposable = vscode.commands.registerCommand('dy-btn.action', async (key) => {
+        if (!key) {
+            // 新增按钮页面
+            return showAddDialog();
+        }
+        let button = actionMap.get(key);
+        console.log(button, 123);
         // 执行函数的逻辑
         const fn = `
         async function dy(data, tool){
-            ${btnInfo.action}
+            ${button.action}
         }
         `;
+        // 如果是对话框
+        if(button?.dialog){
+            
+        }
         const func = new Function('data', 'tool', fn + '\ndy(data, tool);');
-        func(btnInfo, tool);
-	});
+        let data = button?.dialog?.data;
+        func(data, tool);
+    });
+    context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
-    vscode.commands.executeCommand('dy-btn.helloWorld');
+
+    // 更新状态栏项的颜色
+    updateStatusBarItemColor();
 }
 
-export function deactivate() {}
+// 更新状态栏项的颜色
+function updateStatusBarItemColor() {
+    // console.log(vscode.window.activeColorTheme, 1243);
+}
+
+
+export function deactivate() { }
