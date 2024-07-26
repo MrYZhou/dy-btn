0f3443d45d7acfa27c196460ec903edd9ea3375c
larry <1762861715@qq.com>
2024-07-26 15:36:23 +0800

    1

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,9 @@ import * as vscode from 'vscode';
 import * as tool from './tool';
 import * as data from './btn.json';
 
-
 let actionMap = new Map();
 function initBtn() {
-    // 从配置读取
+    // todo 从配置读取
     let myButton;
     data.btnList.forEach(item => {
         myButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
@@ -21,49 +20,36 @@ function initBtn() {
         myButton.show();
         actionMap.set(item.id, item);
     });
-
-
 }
-function showAddDialog() {
-
-}
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
     // 展示动态按钮
     initBtn();
-
     // 注册按钮事件
-    let disposable = vscode.commands.registerCommand('dy-btn.action', async (key) => {
+    context.subscriptions.push(vscode.commands.registerCommand('dy-btn.action', async (key) => {
         if (!key) {
-            // 新增按钮页面
-            return showAddDialog();
+            return  tool.showMessage('请前往配置文件配置');
         }
         let button = actionMap.get(key);
-        console.log(button, 123);
         // 执行函数的逻辑
-        const fn = `
-        async function dy(data, tool){
-            ${button.action}
-        }
-        `;
+        const fn = `async function dy(data, tool){${button.action}}`;
         // 如果是对话框
-        if(button?.dialog){
-            
+        if (button?.dialog) {
+            // todo 收集对话框参数
+            for (let index = 1; index <= 2; index++) {
+                let input = await vscode.window.showInputBox({
+                    prompt: '请输入您的名字' + index,
+                    placeHolder: '例如: John Doe'
+                });
+                if (input) {
+                    // 用户输入了数据
+                    vscode.window.showInformationMessage(`Hello, ${input}!`);
+                }
+            }
         }
         const func = new Function('data', 'tool', fn + '\ndy(data, tool);');
         let data = button?.dialog?.data;
         func(data, tool);
-    });
-    context.subscriptions.push(disposable);
-
-
-    // 更新状态栏项的颜色
-    updateStatusBarItemColor();
+    }));
 }
-
-// 更新状态栏项的颜色
-function updateStatusBarItemColor() {
-    // console.log(vscode.window.activeColorTheme, 1243);
-}
-
 
 export function deactivate() { }
