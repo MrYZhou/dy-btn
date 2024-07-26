a8ea1a50eecdf763aaf0f2f464f5b483edd3dd94
lg <1762861715@qq.com>
2024-07-26 12:00:39 +0800

    1

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,11 @@
 
 import * as vscode from 'vscode';
+import * as tool from './tool';
 let btnInfo = {
     action: "+function hello(){console.log(123)}",
     action1:"node 2.ts",
     data: { key: 'name' }
 };
-// let vscode ={}
-let exec = (command:string)=>{
-    console.log(command);   
-};
-
-// 执行函数的逻辑
-// const functionString = `
-// function dy(data, vscode){
-//     vscode(111)
-// }
-// `;
-// const func = new Function('data', 'vscode', functionString + '\ndy(data, vscode);');
-// // 调用函数
-// func(btnInfo, vscode);
 
 function initBtn(){
 
@@ -29,13 +16,23 @@ export function activate(context: vscode.ExtensionContext) {
     initBtn();
 
     // 注册按钮事件
-	let disposable = vscode.commands.registerCommand('dy-btn.helloWorld', () => {
-	
-		vscode.window.showInformationMessage('Hello World from dy-btn!');
+	let disposable = vscode.commands.registerCommand('dy-btn.helloWorld', async () => {
+        await tool.execFn('echo 1');
+       
+       
+
         // 执行函数的逻辑
+        const fn = `
+        function dy(data, tool){
+            tool.showMessage('Hello World!');
+        }
+        `;
+        const func = new Function('data', 'tool', fn + '\ndy(data, tool);');
+        func(btnInfo, tool);
 	});
 
 	context.subscriptions.push(disposable);
+    vscode.commands.executeCommand('dy-btn.helloWorld');
 }
 
 export function deactivate() {}
