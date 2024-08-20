fe7577c0c0e46f891f60bdc915c2638b65284d2e
larry <1762861715@qq.com>
2024-08-20 21:02:42 +0800

    默认配置示例

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,9 @@ let actionMap = new Map();
 type Btn = { name: string; icon: string; id: string; action: string; data: any[] };
 function initBtn() {
     // 从配置读取
-    let config = vscode.workspace.getConfiguration('dy-btn', vscode.ConfigurationTarget.Global as any);
-
+    let list = initConfig()
     let myButton;
-    config.list.forEach((item: Btn) => {
+    list.forEach((item: Btn) => {
         myButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
         myButton.tooltip = item.name;
         myButton.text = `${item.icon}`;
@@ -25,12 +24,36 @@ function initBtn() {
     });
 }
 function initConfig() {
+    let config = vscode.workspace.getConfiguration('dy-btn', vscode.ConfigurationTarget.Global as any);
+    if (!config) {
+        vscode.workspace.getConfiguration().update('dy-btn', {
+            "list": []
+        }, vscode.ConfigurationTarget.Global);
+        config = vscode.workspace.getConfiguration('dy-btn', vscode.ConfigurationTarget.Global as any);
+    }
+    if (!config.list || config.list.length == 0) {
+        let list = [{
+            "id": 1,
+            "name": "打开仓库",
+            "icon": "$(github)",
+            "action": "exec(`start ${await exec('git config --get remote.origin.url',data.workspaceDir)}`)"
+        },
+        {
+            "id": 2,
+            "icon": "$(run)",
+            "action": "msg(data.path);",
+            "data": [
+                { "label": "执行脚本", "key": "path", "placeholder": "请输入脚本路径" }
+            ]
+        }]
+        config.update('list', list, vscode.ConfigurationTarget.Global);
+        return list
+    }
+    return config.list;
 
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-    // 初始化配置示例
-    initConfig()
     // 展示动态按钮
     initBtn();
     // 注册按钮事件
